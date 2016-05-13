8d3645be80c80fc9550517497b37d1ff72aca364
Akbar Hirani <akbar.hirani123@gmail.com>
2016-05-13 14:03:46 -0400

    Spring WebFlow + Creating a Flow Registry + Hooking up urls to Webflows + Connecting WebFlows & Apache Tiles
    — System for organizing the sequences of web pages (ex:
    Shopping/checkout/continue Shopping, hotel booking, wizard setups, etc)
    — our web flow - instead of displaying email, make a contact us button
    to send message
    
    Create:
    -Dependency - springFrameWork.webflow
    -offer-servlet.xml - Namespace - webflow-config
    -FlowTab - insert flow-registry - id=flowRegistry,
    base-path=/WEB-INF/flows
    -new folder flows
    -in flow-registry - insert flow-location, id=(URL the user goes to, to
    access the webflow), path=context-flow.xml
    
    -create contact-flow.xml in flows folder ->spring web flow definition
    file .xml
    -view-state = transition between states, asps, actions, or any web-flows
    
    Hooking up urls to web flows:
    -flow-registry->contains urls->how to we execute it?
    -insert flow-executor, id=flowExecutor, flow-Registry=flowRegistry
    
    -Now we need a controller to execute
    -new bean-id=flowHandlerAdapter=class
    -insert property, name=flowExecutor=ref
    
    -now we need to map the flow URLs
    -new bean-id=flowHandlerMapping=class
    -insert property-name=flowRegistry=ref
    -insert property- name=order, value=0
    
    Connecting Webflow and Apache Tiles
    -in flowRegistry->flow-builder-serviced=flowBuilderServices
    ->insert flow-builder-services, id=flowBuilderServices,
    view-factory-creator=mvcViewFactoryCreator
    
    -new bean = mvcViewFactoryCreator = class
    -insert property - name=viewResolvers, ref=tilesViewResolver
    
    -create the tsp, place it in tiles ->add the tiles definition in
    default.xml
    -name=“x”->contact-flow.xml - viewstate=“x”
    -the tsp name and view state DOESNOT refer to the URL
    -the flow -location in flow-registry does.

---
+++ src/com/caveofprogramming/spring/web/controllers/LoginController.java
@@ -0,0 +1,85 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.caveofprogramming.spring.web.dao.FormValidationGroup;
+import com.caveofprogramming.spring.web.dao.User;
+import com.caveofprogramming.spring.web.service.UsersService;
+
+@Controller
+public class LoginController {
+
+	private UsersService usersService;
+
+	@Autowired
+	public void setOffersService(UsersService usersService) {
+		this.usersService = usersService;
+	}
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+
+	@RequestMapping("/admin")
+	public String showAdmin(Model model) {
+		
+		List<User> users = usersService.getAllUsers();
+		
+		model.addAttribute("users", users);
+		
+		return "admin";
+	}
+	
+	@RequestMapping("/loggedout")
+	public String showLoggedOut() {
+		return "loggedout";
+	}
+	
+	@RequestMapping("/denied")
+	public String showDenied() {
+		return "denied";
+	}
+	
+	@RequestMapping("/newaccount")
+	public String showNewAccount(Model model) {
+
+		model.addAttribute("user", new User());
+		return "newaccount";
+	}
+
+	@RequestMapping(value = "/createaccount", method = RequestMethod.POST)
+	public String createAccount(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "newaccount";
+		}
+
+		user.setEnabled(true);
+		user.setAuthority("ROLE_USER");
+
+		if (usersService.exists(user.getUsername())) {
+			result.rejectValue("username", "DuplicateKey.user.username");
+			return "newaccount";
+		}
+		try {
+			usersService.create(user);
+		} catch (DuplicateKeyException e) {
+
+			result.rejectValue("username", "DuplicateKey.user.username",
+					"This username already exists!");
+			return "newaccount";
+		}
+
+		return "accountcreated";
+	}
+}
